164c42911b4cbdec3eeb70798d4193c390cc26b7
mago mahi <maheshhonnamurthy099@gmail.com>
2023-01-16 18:06:14 +0530

    [Mahesh]Add: Database Created

---
+++ Employee-Payroll/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll.Repository
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Click Below Options:\n1.CreateDatabase");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    EmployeeRepository employeeRepository = new EmployeeRepository();
+                    employeeRepository.CreateDatabase();
+                    break;
+            }
+         
+        }
+    }
+}
+       
---
+++ Employee-Payroll/Repository/EmployeeRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll.Repository
+{
+    public class EmployeeRepository
+    {
+        public void CreateDatabase()
+        {
+            try
+            {
+                SqlConnection connectin = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial catalog=master;
+                Integrated Security=true");
+                connectin.Open();
+                SqlCommand cmd = new SqlCommand("create database EmpPayroll", connectin);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Database created successfully");
+                Console.WriteLine("Connection is established with database");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Connection is not established with database");
+            }
+        }
+    }
+}
+
+    
